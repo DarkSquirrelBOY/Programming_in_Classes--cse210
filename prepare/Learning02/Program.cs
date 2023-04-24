4b98fbd151740446b06b465455541dc13629b637
cadensdavis <c.d.stewart333@gmail.com>
2023-04-24 14:48:29 -0600

    practing in class

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -8,6 +8,11 @@ namespace Learning02
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Learning02 World!");
+            List<int> myList = new List<int>();
+            myList.Add(121);
+            myList.Add(2021);
+            myList.Remove(0);
+            
         }
     }
 }
