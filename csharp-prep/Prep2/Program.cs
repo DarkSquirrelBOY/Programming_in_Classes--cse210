fcbae9c2197e0d05dff40fd1042d465ed5534ea8
cadensdavis <c.d.stewart333@gmail.com>
2023-04-24 18:33:52 -0600

    completed stretch challendges

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -10,27 +10,61 @@ namespace gradeinput
             Console.Write("What is your current grade in this class? ");
             string grade = Console.ReadLine();
             int number = int.Parse(grade);
+            string lettergrade;
 
             if (number >=  90)
             {
-                Console.WriteLine("A");
+                lettergrade = "A";
+                //Console.Write("A");
             }
             else if (number >= 80 && number < 90)
             {
-                Console.WriteLine("B");
+                lettergrade = "B";
+                //Console.Write("B");
             }
             else if (number >= 70 && number < 80)
             {
-                Console.WriteLine("C");
+                lettergrade = "C";
+                //Console.Write("C");
             }
             else if (number >= 60 && number < 70)
             {
-                Console.WriteLine("D");
+                lettergrade = "D";
+                //Console.Write("D");
             }
             else if (number < 60)
             {
-                Console.WriteLine("F");
+                lettergrade = "F";
+                //Console.Write("F");
             }
+            else
+            {
+                lettergrade = "";
+            }
+
+            float remainder;
+            remainder = number % 10;
+
+            // Console.WriteLine($"{remainder}");
+
+            string sign;
+
+            if (remainder >= 7 && number < 97 && number > 60)
+            {
+                sign = "+";
+            }
+                
+            else if (remainder < 3 && number > 60)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "";
+            }
+
+            Console.Write($"{lettergrade}{sign}");
+
         }
     }
 }
