8f50274b493534b0665584e28f3a49e3b17c508c
cadensdavis <c.d.stewart333@gmail.com>
2023-04-24 18:07:11 -0600

    made namespace and completed core requiremets

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,16 +1,36 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace gradeinput
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.Write("What is your current grade in this class? ");
-        string grade = Console.ReadLine();
-        int number = int.Parse(grade);
-
-        if (number > 90)
+        static void Main(string[] args)
         {
-            Console.WriteLine("A");
+            Console.Write("What is your current grade in this class? ");
+            string grade = Console.ReadLine();
+            int number = int.Parse(grade);
+
+            if (number >=  90)
+            {
+                Console.WriteLine("A");
+            }
+            else if (number >= 80 && number < 90)
+            {
+                Console.WriteLine("B");
+            }
+            else if (number >= 70 && number < 80)
+            {
+                Console.WriteLine("C");
+            }
+            else if (number >= 60 && number < 70)
+            {
+                Console.WriteLine("D");
+            }
+            else if (number < 60)
+            {
+                Console.WriteLine("F");
+            }
         }
     }
 }
