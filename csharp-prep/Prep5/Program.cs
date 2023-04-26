fd890362e93578274689b3fa5188d45361d6b354
cadensdavis <c.d.stewart333@gmail.com>
2023-04-26 11:36:29 -0600

    completed prep 5 and all requiremnets were met

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,55 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace ListManipulation
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+
+            DisplayWelcome();
+
+            string username = PromptUserName();
+
+            int usernumber = PromptUserNumber();
+
+            int squarednumber = SquareNumber(usernumber);
+
+            DisplayResults(username, squarednumber);
+
+
+            void DisplayWelcome()
+            {
+                Console.WriteLine("Welcome to the Program!");
+            }
+
+            static string PromptUserName()
+            {
+                Console.Write("What is your name? ");
+                string username = Console.ReadLine();
+                return username;
+            }
+
+            static int PromptUserNumber()
+            {
+                Console.Write("What is your favorite number? ");
+                string number = Console.ReadLine();
+                int usernumber = int.Parse(number);
+                return usernumber;
+            }
+
+            static int SquareNumber(int number)
+            {
+                int squarednumber = number * number;
+                return squarednumber;
+            }
+
+            void DisplayResults(string username ,int usernumber)
+            {
+                Console.WriteLine($"{username}, the square of your number is {usernumber}");
+            }
+        }
     }
 }
