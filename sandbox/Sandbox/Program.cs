1a02f8ae28ab28bec0039505aa2b427466159057
cadensdavis <c.d.stewart333@gmail.com>
2023-04-25 19:07:22 -0600

    sandbox things

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,10 +1,26 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        //testing
-        Console.WriteLine("Hello Sandbox World! ");
+        List<int> numbers = new List<int>();
+        
+        // Please note we could use a do-while loop here instead
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+            
+            // Only add the number to the list if it is not 0
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
     }
 }
